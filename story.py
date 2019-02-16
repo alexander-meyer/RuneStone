c79fe99ad3abe2fc0ef57682880094155be6b6a2
alexander-meyer <alex.7.meyer@gmail.com>
2019-02-16 14:39:14 -0500

    Incorporate battle mechanic (in a test capacity). Refine getCommand method to accept Character as parameter, allowing for mutability of player attributes.

--- story.py
+++ story.py
@@ -1,5 +1,4 @@
 import random, math
-from fight import Battle
 
 #      #
 # DATA #
@@ -10,20 +9,20 @@ from fight import Battle
 rooms = {
     'meadow': {'name': 'meadow', 'description': 'find yourself in a meadow', 'exits': {'north', 'west', 'east'},
                'north': 'hill', 'west': 'forest',
-               'east': 'river', 'text': "A gentle breeze blows through, causing the grass to dance and sway." },
-    'river': {'name': 'river', 'description': 'come to the edge a river', 'exits': {'west'},
+               'east': 'river', 'text': "A gentle breeze blows through, causing the grass to dance and sway.", },
+    'river': {'name': 'river', 'description': 'come to the bank a river', 'exits': {'west'},
               'west': 'meadow', 'text': "The water looks cool and pleasant.", 'item': 'Amethyst ring', 'event': 'swim'},
-    'forest': {'name': 'forest', 'description': 'arrive at the entrance to a forest', 'exits': {'east'}, 'event': 'illuminate',
-               'east': 'meadow',
-               'text': "Looming pines make it too dark to see - perhaps if there was a way to illuminate the path?",
+    'forest': {'name': 'forest', 'description': 'stand at the entrance to a forest', 'exits': {'east'}, 'event': 'illuminate',
+               'east': 'meadow', 'text': "Looming pines make it too dark to see - perhaps if there was a way to illuminate the path?",
                'items': 'none'},
     'cabin': {'name': 'cabin', 'description': 'stumble upon an old cabin', 'exits': {'north'}, 'north': 'forest',
               'text': "Inside you see old bottles, rotting furniture and various papers strewn across the floor."},
     'hill': {'name': 'hill', 'description': 'reach the top of a small hill', 'exits': {'north', 'south'}, 'south': 'meadow',
-             'north': 'town',
-             'text': "The land stretches out for leagues ahead of you. A mountain looms in the horizon, dark and foreboding."},
-    'town': {'name': 'town', 'description': 'arrive at a bustling town', 'exits': {'north', 'south', 'east'},
-             'south': 'hill', 'north': 'mountain', 'east': 'plains',
+             'north': 'road', 'text': "The land stretches out for leagues ahead of you. A mountain looms in the horizon, dark and foreboding."},
+    'road': {'name': 'road', 'description': 'arrive at a cobblestone road', 'exits': {'north', 'south'}, 'north': 'town', 'south': 'hill',
+             'text': 'The path is well worn, telling of countless travelers before you.','event': 'fight', 'enemies': 1 },
+    'town': {'name': 'town', 'description': 'come to a bustling town', 'exits': {'north', 'south', 'east'},
+             'south': 'road', 'north': 'mountain', 'east': 'plains',
              'text': 'A bell rings in the distance, barely discernible amidst the market bustle.'},
     'plains': {'name': 'plains'}
 }
@@ -45,8 +44,6 @@ currentRoom = rooms['meadow']
 
 # For the 'go back' command, keep track of previous room
 prevRoom = None
-# For the 'check status' command, made global since 'player' object not in scope of getCommand
-status = None
 # Player inventory, accessible with 'check bag' command
 inventory = []
 
@@ -143,6 +140,10 @@ class Character:
         # determine percentage chance of evasion
         return dex >= random.randint(0,100)
 
+    # Restore method to be used by subclasses
+    def restore(self):
+        raise NotImplementedError("Hasn't been implemented!")
+
 
 
     # toString method
@@ -160,14 +161,22 @@ class Rogue(Character):
     def __init__(self):
         Character.__init__(self,name = "", build = "Rogue", health = 16, attk = 7, defense = 3, evade = 3)
 
+    def restore(self):
+        self.health = 16
+
 class Mage(Character):
     def __init__(self):
         Character.__init__(self,name = "", build = "Mage", health = 12, attk = 9, defense = 2, evade = 2)
+
+    def restore(self):
+        self.health = 12
 
 class Warrior(Character):
     def __init__(self):
         Character.__init__(self,name = "", build = "Warrior", health = 19, attk = 6, defense = 6, evade = 1)
 
+    def restore(self):
+        self.health = 19
 
 # Enemy classes
 class Spider(Character):
@@ -178,7 +187,7 @@ class Spider(Character):
 class Bandit(Character):
 
     def __init__(self):
-        Character.__init__(self, name= "Bandit", build = "Enemy", health = 6, attk = 4, defense = 2, evade = 2)
+        Character.__init__(self, name= "Bandit", build = "Enemy", health = 10, attk = 6, defense = 3, evade = 2)
 
 class Sorcerer(Character):
 
@@ -229,10 +238,17 @@ def goInCommand(command):
         else:
             print("\nYou can't go there.\n")
 
+def badCommand():
+
+    print("\nUnsure what you mean.\n")
+
 
 # A series of if/elif paths to capture various user commands
-def getCommand(command):
+def getCommand(Character):
 
+    print()
+    command = input("What do you do?\n\n> ")
+    command = command.strip().lower()
 
     # MOVEMENT
 
@@ -289,9 +305,7 @@ def getCommand(command):
 
     elif command in ("check health", "check status", "status","health"):
         print()
-        global status
-        print(status)
-        print()
+        print(Character)
 
 
     # BAG
@@ -313,27 +327,42 @@ def getCommand(command):
     elif command in ("sleep", "lie down", "lay down", "rest", "to sleep", "take a nap", "nap"):
         if currentRoom == rooms['meadow']:
             print("\n{}\n\n".format(events['rest']['text']))
+            Character.restore()
+        else:
+            badCommand()
+
 
     # Swimming event, finding amethyst ring
     elif command in ("swim","dive in","swimming","for a swim","in river", "jump in"):
         if currentRoom == rooms['river']:
             print("\n{}\n\n".format(events['swim']['text']))
-        global inventory
-        if not 'Amethyst Ring' in inventory:
-            print("Something shiny catches your eye on the riverbed.\n\n * Amethyst Ring added to inventory *\n")
-            inventory.append('Amethyst Ring')
+            global inventory
+            if not 'Amethyst Ring' in inventory:
+                print("Something shiny catches your eye on the riverbed.\n\n * Amethyst Ring added to inventory *\n")
+                inventory.append('Amethyst Ring')
+        else:
+            badCommand()
+
+
 
     # MISC
 
     elif "smoke" in command:
         print("\nAlas. You're all out.\n")
 
+    elif "dance" in command:
+        print("\nLeaping about, you let your rhythm burst out in frenzied dance. Nothing happens, but you feel great.")
+
+
     # BAD COMMAND
 
     else:
-        print("\nUnsure what you mean.\n")
+        badCommand()
 
 
+    # format
+    print()
+    print(".\n.\n.\n\n")
 
 
 
@@ -344,56 +373,56 @@ def getCommand(command):
 
 class Battle:
 
-    def startFight(self, char1, char2):
+    def startFight(self, player, char2):
 
 
         while True:
-            if self.getAttackResult(char1, char2) == "Game Over":
-                print("Game Over")
+            if self.getAttackResult(player, char2) == "Over":
+                print("Victorious!")
                 break
 
-            if self.getAttackResult(char2, char1) == "Game Over":
-                print("Game Over")
+            if self.getAttackResult(char2, player) == "Over":
+                print("Game Over!")
                 break
 
 
     # Make this method static because we don't need to use self
-
     @staticmethod
     def getAttackResult(charA, charB):
-        charADamage = charA.attack()
 
-        charBBlockAmt = charB.block()
-
-        damage2CharB = math.ceil(charADamage - charBBlockAmt)
-
-        charB.health = charB.health - damage2CharB
-
-        print("{} attacks {} and deals {} damage".format(charA.name, charB.name, damage2CharB))
-
-        print("{} is down to {} health".format(charB.name, charB.health))
+        # check to see if dodge
+        if charB.dodge():
+            print("{} attacks {}... {} dodges!".format(charA.name,charB.name, charB.name))
+        # else calculate damage
+        else:
+            damage = math.ceil(charA.attack() - charB.block())
+            if damage < 0: damage = 0
+            print("{} attacks {} and deals {} damage".format(charA.name,charB.name, damage))
+            charB.health = charB.health - damage
+            if charB.health < 0: charB.health = 0
+        # current status
+        print("{} is down to {} health".format(charB.name,
+                                               charB.health))
 
         if charB.health <= 0:
-            print("{} has Died and {} is Victorious".format(charB.name, charA.name))
-
-            return "Game Over"
+            return "Over"
         else:
-            return "Fight Again"
+            return "Again"
 
 
 
-# STORY #
 #       #
+# STORY #
 #       #
 
 class Story:
     print("\n   _____________________________________________________________________________________")
-    print("  | rrrrrr    u     u   n     n   eeeeee    ssss   ttttttt   ooooo    n     n   eeeeee  |  ")
-    print("  | r     r   u     u   n n   n   e        s          t     o     o   n n   n   e       |  ")
-    print("  | rrrrrr    u     u   n  n  n   eeeeee    sss       t     o     o   n  n  n   eeeeee  |  ")
-    print("  | r    r    u     u   n   n n   e            s      t     o     o   n   n n   e       |  ")
-    print("  | r     r    uuuuu    n     n   eeeeee   ssss       t      ooooo    n     n   eeeeee  |  ")
-    print("  |_____________________________________________________________________________________|  ")
+    print("  | rrrrrr    u     u   n     n   eeeeee    ssss   ttttttt   ooooo    n     n   eeeeee  |           ")
+    print("  | r     r   u     u   n n   n   e        s          t     o     o   n n   n   e       |           ")
+    print("  | rrrrrr    u     u   n  n  n   eeeeee    sss       t     o     o   n  n  n   eeeeee  |           ")
+    print("  | r    r    u     u   n   n n   e            s      t     o     o   n   n n   e       |              ")
+    print("  | r     r    uuuuu    n     n   eeeeee   ssss       t      ooooo    n     n   eeeeee  |          ")
+    print("  |_____________________________________________________________________________________|        ")
 
 
     print("\n\n *\n *\n *\n")
@@ -417,8 +446,8 @@ class Story:
 
         elif choice == "info":
             print("\n\nWarrior: strong and tough, you have trained yourself well to survive hardship. Well-balanced in attack and defense.\n"
-                      "Mage:    harnessing ancient power, your spells overwhelm those in your path. Strong attack, though weak defenses.\n"
-                      "Rogue:   smooth as a whisper, your stealth lets you deal damage before the enemy knows what's happening. Good strength and evasion.\n")
+                      "Mage:    harnessing an ancient power, your spells overwhelm those in your path. Powerful attack with weaker defense.\n"
+                      "Rogue:   smooth as a whisper, your stealth lets you strike before enemies know what hit them. Good strength and evasion.\n")
 
         else:
             print("\nNot sure what you mean - pick one of the selected options.\n")
@@ -427,8 +456,8 @@ class Story:
     # Take user name and set for player object
     print()
     player.name = input("Your name?\n\n> ")
-    global status
-    status = player
+    # global status
+    # status = player
     print()
 
     print("\nEnter commands to interact with the world (type 'help' for some hints)\n\n\n*\n*\n*\n\n")
@@ -442,13 +471,17 @@ class Story:
         print(currentRoom['text'])
         print()
 
+        # Check for event occurance
+        if 'event' in currentRoom:
+            if currentRoom['event'] == 'fight' and currentRoom['enemies'] > 0:
+                currentRoom['enemies'] -= 1
+                battle = Battle()
+                bandit = Bandit()
+                battle.startFight(player,bandit)
+
         # Get user commands
-        print()
-        command = input("What do you do?\n\n> ")
-        command = command.strip().lower()
-        getCommand(command)
-        print()
-        print(".\n.\n.\n")
+        getCommand(player)
+
 
 
 def main():
