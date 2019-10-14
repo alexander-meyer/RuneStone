0fe553948f6cf8cdc63f2732715dc796f0f6b498
alexander-meyer <alexander.meyer@mail.mcgill.ca>
2019-10-14 11:53:16 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+var rooms = {
+    meadow: {
+        description:
+            "<p>You find yourself in a meadow. A gentle breeze blows through, causing the grass to dance and sway.</p>",
+        directions: {
+            north: "hill",
+            west: "forest",
+            east: "river"
+        }
+    },
+    river: {
+        description:
+            "<p>You come to the bank of a river. The water looks cool and pleasant.</p>",
+        directions: {
+            west: "meadow"
+        },
+        item: "Amethyst ring",
+        event: "swim"
+    },
+    forest: {
+        description:
+            "<p>You stand at the entrance to a forest. Looming pines make it too dark to see - perhaps if there was a way to illuminate the path?</p>",
+        directions: {
+            east: "meadow",
+            south: "cabin"
+        },
+        event: "illuminate"
+    },
+    cabin: {
+        description:
+            "<p>You stumble upon an old cabin. Inside you see old bottles, rotting furniture and various papers strewn about.<p>",
+        directions: {
+            north: "forest"
+        }
+    },
+    hill: {
+        description:
+            "<p>You reach the top of a small hill. The land stretches out in front of you. A mountain looms on the horizon, dark and foreboding.<p>",
+        directions: {
+            north: "road",
+            south: "meadow"
+        }
+    },
+    road: {
+        description:
+            "<p>You come to a well-trodden road. No doubt many travellers have made their way through here.<p>",
+        directions: {
+            north: "town",
+            south: "hill"
+        }
+    },
+    town: {
+        description:
+            "<p>You come to a bustling town. A bell rings in the distance, barely discernible amidst the bustle of a day market.",
+        directions: {
+            south: "road"
+        }
+    }
+};
+
+$(document).ready(function() {
+    $("#game-text").append("<p>" + rooms.meadow.description + "</p>");
+
+    $(document).keypress(function(key) {
+        if (key.which === 13 && $("#user-input").is(":focus")) {
+            var value = $("#user-input").val();
+            alert(value);
+        }
+    });
+});
