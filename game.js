b9952e205b8a86b5e838c1f8ec87ec9cc0885cd2
alexander-meyer <alexander.meyer@mail.mcgill.ca>
2020-01-13 18:44:34 -0800

    add switch statement for cardinal directions

--- game.js
+++ game.js
@@ -1,11 +1,39 @@
-$(document).ready(function () {
+var currentRoom = "start";
 
-    $('#game-text').append(rooms.start.description)
+var gameText = $('#game-text');
+var userInput = $('#user-input');
+
+function changeRoom(dir) {
+    if ((rooms[currentRoom].directions[dir]) !== undefined) {
+        currentRoom = rooms[currentRoom].directions[dir]
+    }
+    gameText.append(rooms[currentRoom].description)
+}
+
+$(document).ready(function () {
+    $('#game-text').append(rooms[currentRoom].description)
 
     $(document).keypress(function (key) {
         if ((key.which === 13) && $('#user-input').is(':focus')) {
-            var value = $('#user-input').val();
-            alert(value);
+            var value = $('#user-input').val().toLowerCase();
+            $('#user-input').val('')
+
+            switch (value) {
+                case "north": case "up":
+                    changeRoom("north");
+                    break;
+                case "south": case "down":
+                    changeRoom("south");
+                    break;
+                case "west": case "left":
+                    changeRoom("west");
+                    break;
+                case "east": case "right":
+                    changeRoom("east");
+                    break
+                default:
+                    gameText.append("<br /> Not sure what you mean.");
+            }
         };
     })
 })
